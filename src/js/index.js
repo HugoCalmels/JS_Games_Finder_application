ab76c795e5cb7e3034febf0c1629e45a8c39651d
Hugo Calmels <calmels.hugo@yahoo.fr>
2022-03-22 18:03:00 +0100

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+console.log("hi")
+
+const nav = `<a href="/">Home</a> | 
+                   <a href="/about">About</a> | 
+                   <a href="/contact">Contact</a>`;
+      const routes = {
+        "/": `<h1>Home</h1>${nav}<p>Welcome home!</p>`,
+        "/about": `<h1>About</h1>${nav}<p>This is a tiny SPA</p>`,
+      };
+      const render = path => {
+        document.querySelector("#app")
+          .innerHTML = routes[path] || `<h1>404</h1>${nav}`
+        ;
+        document.querySelectorAll('[href^="/"]').forEach(el => 
+          el.addEventListener("click", evt => {
+            evt.preventDefault();
+            const {pathname: path} = new URL(evt.target.href);
+            window.history.pushState({path}, path, path);
+            render(path);
+          })
+        );
+      };
+      window.addEventListener("popstate", e =>
+        render(new URL(window.location.href).pathname)
+      );
+      render("/");
